ac1abec8847d040bc0c1d7c39b594d04beb4bb08
Air <69145482+AiryAir@users.noreply.github.com>
2023-04-04 00:13:02 +0530

    left, right wheel vel

--- scripts/controls.py
+++ scripts/controls.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+
+d = 1/2         # distance between wheels
+vl = 0          # linear velocity of left wheel
+vr = 0          # linear velocity of right wheel
+
+vel = Twist()   # velocity message
+
+def message_callback(vel):
+    vl = vel.linear.x - vel.angular.z*d/2
+    vr = vel.linear.x + vel.angular.z*d/2
