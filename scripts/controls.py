62bedc636b5461bb296c5d3825442f7fa3638be8
Air <69145482+AiryAir@users.noreply.github.com>
2023-04-04 14:09:04 +0530

    added publisher

--- scripts/controls.py
+++ scripts/controls.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# code for differential drive robot controlled by teleop_twist_keyboard
 
 import rospy
 from geometry_msgs.msg import Twist
@@ -8,8 +9,21 @@ d = 1/2         # distance between wheels
 vl = 0          # linear velocity of left wheel
 vr = 0          # linear velocity of right wheel
 
-vel = Twist()   # velocity message
+def move():
+    a = 0
+    rospy.init_node('scobo_med',anonymous=True)
+    twist = Twist()
+    pub = rospy.Publisher('/scobo/cmd_vel', Twist, queue_size=10)
+    twist = Twist()
 
-def message_callback(vel):
-    vl = vel.linear.x - vel.angular.z*d/2
-    vr = vel.linear.x + vel.angular.z*d/2
+    while(1):
+        a = a+1
+        a=0
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        #Testing our function
+        move()
+        
+    except rospy.KeyboardInterrupt: pass
